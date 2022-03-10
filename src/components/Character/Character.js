84774b9668f7c3d3edaa5d81ea9102e0875922c6
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-09 17:01:52 -0800

    dropdown and Character page in view

---
+++ src/components/Character/Character.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Character() {
+  return (
+    <div className="right">
+      <div className="character">
+        <div className="bg head" id="head"></div>
+        <div className="bg middle" id="middle"></div>
+        <div className="bg bottom" id="bottom"></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Dropdown from '../Dropdown/Dropdown';
+import Character from '../Character/Character';
+
+export default function Main() {
+  return (
+    <main>
+      <Dropdown />
+      <Character />
+    </main>
+  );
+}
