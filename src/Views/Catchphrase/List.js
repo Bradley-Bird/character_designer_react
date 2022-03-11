0c85f2c96b47e92149fd1549fc8451ad06f12ac8
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-11 15:46:23 -0800

    fixed issue with key, and catchphrase being inported when not needed

--- src/Views/Catchphrase/List.js
+++ src/Views/Catchphrase/List.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function List({ catchphraseArray, catchphrase }) {
+export default function List({ catchphraseArray }) {
   return (
     <div>
       <ul id="catchphrases">
-        {catchphraseArray.map((catchphrase, idx) => (
-          <li key={idx}>{catchphrase}</li>
+        {catchphraseArray.map((catchphrase) => (
+          <li key={catchphrase}>{catchphrase}</li>
         ))}
       </ul>
     </div>
